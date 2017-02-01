d5b0fefba890dfd323d4c7a2505db4dbbdcc1495
Pranav Bhandari <pbhanda1@bl-n30.local>
2017-02-01 16:15:12 -0500

    first commit

---
+++ Memorable Places/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by Pranav Bhandari on 2/1/17.
+//  Copyright © 2017 Pranav Bhandari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
